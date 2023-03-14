a10263edecd27ebcfed34f07b2ab3b5449c0884f
Oscar Olivo <oscar.olivo@ceiba.com.co>
2023-03-13 23:43:43 -0500

    adicionando base64 encoder y sleuth observabilidad

--- src/main/java/com/formacionbdi/springboot/app/zuul/oauth/ResourceServerConfig.java
+++ src/main/java/com/formacionbdi/springboot/app/zuul/oauth/ResourceServerConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 
 @RefreshScope
 @Configuration
@@ -74,7 +75,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter(){
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey(jwtKey);
+        tokenConverter.setSigningKey(Base64.getEncoder().encodeToString(jwtKey.getBytes()));
         return tokenConverter;
     }
 }
